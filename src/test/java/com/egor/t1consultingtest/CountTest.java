6f7ee95e8965c423d2a1e855d920b8fdfce748d5
Egor Gavrilov <gavrilovegor519@gmail.com>
2023-10-28 16:27:27 +0300

    Refactor test mechanism. I'm now using unit testing.

--- src/test/java/com/egor/t1consultingtest/CountTest.java
+++ src/test/java/com/egor/t1consultingtest/CountTest.java
@@ -1,37 +1,35 @@
 package com.egor.t1consultingtest;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import com.egor.t1consultingtest.service.impl.CountServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import java.nio.charset.Charset;
+import java.util.Map;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
 public class CountTest {
-    @Autowired
-    private MockMvc mvc;
+    private CountServiceImpl countService;
+
+    @BeforeEach
+    public void preTestSetup() {
+        countService = new CountServiceImpl();
+    }
 
     @Test
-    public void testWithSampleValue() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.post("/count")
-                        .content("aaaaabcccc")
-                        .characterEncoding(Charset.defaultCharset())
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content()
-                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("a").value(5))
-                .andExpect(jsonPath("b").value(1))
-                .andExpect(jsonPath("c").value(4));
+    public void test1() {
+        Map<Character, Integer> charMap = countService.count("aaaaabcccc");
+
+        assertEquals(charMap.get('a'), 5);
+        assertEquals(charMap.get('b'), 1);
+        assertEquals(charMap.get('c'), 4);
+    }
+
+    @Test
+    public void test2() {
+        Map<Character, Integer> charMap = countService.count("");
+
+        assertTrue(charMap.isEmpty());
     }
 }
